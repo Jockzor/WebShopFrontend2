d57c97e65dcf0e1fe72e8b7cbfc8caf29981daf1
Joacim Edlund <jolleedlund@hotmail.com>
2024-01-24 22:17:32 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Router>
+      <Routes>
+        <Route/>
+      </Routes>
+    </Router>
+      
+  )
+}
+
+export default App
