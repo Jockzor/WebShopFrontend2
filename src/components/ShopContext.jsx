16f08a0380e35e3d5b18ba9f05d4d362a7be3bf7
Joacim Edlund <jolleedlund@hotmail.com>
2024-02-10 16:32:15 +0100

    Ny Cart

--- src/components/ShopContext.jsx
+++ src/components/ShopContext.jsx
@@ -1,56 +1,69 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState} from "react";
 
 export const ShopContext = createContext(null);
 
+// title, description, price, quantity, image,  productId 
+
+const groupById = (array) => {
+    return array.reduce((accumulator, currentValue) => {
+    // Get the id of the current value
+    const id = currentValue.productId;
+
+    // If the accumulator doesn't have an array for this id, create it
+    if (!accumulator[id]) {
+        accumulator[id] = [];
+    }
+
+    // Add the current value to the array for this id
+    accumulator[id].push(currentValue);
+
+    return accumulator;
+    }, []); // Initial value of the accumulator is an empty object
+}
+
+const removeHelper = (cart, itemId) => {
+    // Find the index of the first item with the given id
+    const index = cart.findIndex(item => item.productId === itemId);
+
+    // If the item is found, remove it from the cart
+    if (index !== -1) {
+        cart.splice(index, 1);
+    }
+
+    return cart;
+}
+
 export const ShopContextProvider = (props) => {
-    const { PRODUCTS } = props; 
+    const [cartItems, setCartItems] = useState([]);
 
-    const getDefaultCart = () => {
-        let cart = {};
-        for (let i = 1; i <= PRODUCTS.length; i++) { 
-            cart[i] = 0;
-        }
-        return cart;
-    };
-
-    const [cartItems, setCartItems] = useState(getDefaultCart());
-
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    const addToCart = (cartItem) => {
+        setCartItems((prev) => ([...prev, cartItem]));
     };
 
     const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for (const item in cartItems) {
-            if (cartItems[item] > 0) {
-                let itemInfo = PRODUCTS.find((product) => product.id === Number(item));
-                totalAmount += cartItems[item] * itemInfo.price;
-            }
-        }
+        const mappedNumbers = cartItems.map(({price}) => price);
+        const totalAmount = mappedNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        
         return totalAmount;
     };
 
     const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+        setCartItems([...removeHelper(cartItems, itemId)]);
     };
-
+    
     const updateCartItemCount = (newAmount, itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
     };
+    
+    const getGroupedcartItemsByProductId = React.useMemo(() => {
+        return groupById(cartItems);
+    }, [cartItems]);
 
-    const contextValue = {
-        cartItems,
-        ShopContext,
-        addToCart,
-        removeFromCart,
-        updateCartItemCount,
-    };
+    const contextValue = {cartItems, addToCart, removeFromCart, updateCartItemCount, getTotalCartAmount, getGroupedcartItemsByProductId};
 
-    return (
+    return (   
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
     );
 };
-
-
